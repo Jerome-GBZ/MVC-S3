057303708d4a53c0c0236bcdd3c2caa0a58559fa
eventdark <darkevent924@gmail.com>
2020-04-02 23:19:39 +0200

    correction loris

--- MVC-S3/Controler/magasins.ctrl.php
+++ MVC-S3/Controler/magasins.ctrl.php
@@ -1,16 +1,15 @@
 <?php
-<<<<<<< HEAD
   $cheminMagasin = "../Model/magasin.class.php";
   $cheminMagasinDAO = "../Model/magasinDAO.class.php";
   $cheminFramework = "../Framework/view.class.php";
   require_once($cheminMagasin);
   require_once($cheminMagasinDAO);
   include($cheminFramework);
-=======
+
   require_once("../Model/magasin.class.php");
   require_once("../Model/magasinDAO.class.php");
   include("../Framework/view.class.php");
->>>>>>> 467ea4866e6c25f6081b8ca88910174591dd0b0a
+
 
   $magasinDAO = new MagasinDao();
 
