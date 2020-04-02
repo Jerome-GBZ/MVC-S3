b9a000b06b17693fb8f804fffcf69138a7c24f9f
eventdark <darkevent924@gmail.com>
2020-04-02 17:30:18 +0200

    erreur loris ragarde

--- MVC-S3/Controler/magasins.ctrl.php
+++ MVC-S3/Controler/magasins.ctrl.php
@@ -1,18 +1,18 @@
 <?php
-  $cheminMagasin = dirname(__FILE__)."/../Model/magasin.class.php";
-  $cheminMagasinDAO = dirname(__FILE__)."/../Model/magasinDAO.class.php";
-  $cheminFramework = dirname(__FILE__)."/../Framework/view.class.php";
+  $cheminMagasin = dirname(__FILE__)."../Model/magasin.class.php";
+  $cheminMagasinDAO = dirname(__FILE__)."../Model/magasinDAO.class.php";
+  $cheminFramework = dirname(__FILE__)."../Framework/view.class.php";
   require_once($cheminMagasin);
   require_once($cheminMagasinDAO);
   include($cheminFramework);
-  
+
   $magasinDAO = new MagasinDao();
 
   for ($i = 1; $i < 5; $i++) {
     $magasins[$i] = $magasinDAO->get($i);
   }
 
-  $view = new View('/../View/magasins.view.php');
+  $view = new View('../View/magasins.view.php');
   $view->magasins = $magasins;
 
   $view->show();
