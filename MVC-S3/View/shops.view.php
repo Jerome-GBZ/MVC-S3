2cae9ba0833430cdbf63bab6e6d3fd12ec66d76a
eventdark <darkevent924@gmail.com>
2020-04-03 13:01:25 +0200

    romain - loris - jerome modif groupe

--- MVC-S3/View/shops.view.php
+++ MVC-S3/View/shops.view.php
@@ -49,109 +49,108 @@
         }
 
         .gris {background-color: #e7e7e7; color: black;} /* Gris */
-/*---------------------------MENU-DEROULANT---------------------------------------*/
+        /*---------------------------MENU-DEROULANT---------------------------------------*/
 
-#menu-deroulant, #menu-deroulant ul{
-padding:0;
-margin:0;
-list-style:none;
-text-align:center;
-}
-#menu-deroulant li{
-display:inline-block;
-position:relative;
-border-radius:8px;
-}
+        #menu-deroulant, #menu-deroulant ul{
+        padding:0;
+        margin:0;
+        list-style:none;
+        text-align:center;
+        }
+        #menu-deroulant li{
+        display:inline-block;
+        position:relative;
+        border-radius:8px;
+        }
 
-#menu-deroulant li:hover{
-border-radius: 8px 8px 0 0;
-}
+        #menu-deroulant li:hover{
+        border-radius: 8px 8px 0 0;
+        }
 
-#menu-deroulant ul li{
-display:inherit;
-border-radius:0;
-}
-#menu-deroulant ul li:hover{
-border-radius:0;
-}
-#menu-deroulant ul li:last-child{
-border-radius:0 0 8px 8px;
-}
-#menu-deroulant ul{
-position:absolute;
-z-index: 1000;
-max-height:0;
-left: 0;
-right: 0;
-overflow:hidden;
--moz-transition: .8s all .3s;
--webkit-transition: .8s all .3s;
-transition: .8s all .3s;
-}
-#menu-deroulant li:hover ul{
-max-height:15em;
-}
-/* background des liens menus */
-#menu-deroulant li:first-child{
-background-color: #e7e7e7;
+        #menu-deroulant ul li{
+        display:inherit;
+        border-radius:0;
+        }
+        #menu-deroulant ul li:hover{
+        border-radius:0;
+        }
+        #menu-deroulant ul li:last-child{
+        border-radius:0 0 8px 8px;
+        }
+        #menu-deroulant ul{
+        position:absolute;
+        z-index: 1000;
+        max-height:0;
+        left: 0;
+        right: 0;
+        overflow:hidden;
+        -moz-transition: .8s all .3s;
+        -webkit-transition: .8s all .3s;
+        transition: .8s all .3s;
+        }
+        #menu-deroulant li:hover ul{
+        max-height:15em;
+        }
+        /* background des liens menus */
+        #menu-deroulant li:first-child{
+        background-color: #e7e7e7;
 
-}
-#menu-deroulant li:nth-child(2){
-background-color: #e7e7e7;
+        }
+        #menu-deroulant li:nth-child(2){
+        background-color: #e7e7e7;
 
-}
-#menu-deroulant li:nth-child(3){
-background-color: #e7e7e7;
-/*background-image:-webkit-linear-gradient(top, #F6AD1A 0%, #9F391A 100%);
-background-image:linear-gradient(to bottom, #F6AD1A 0%, #9F391A 100%);*/
-}
-#menu-deroulant li:last-child{
-background-color: #e7e7e7;
+        }
+        #menu-deroulant li:nth-child(3){
+        background-color: #e7e7e7;
+        /*background-image:-webkit-linear-gradient(top, #F6AD1A 0%, #9F391A 100%);
+        background-image:linear-gradient(to bottom, #F6AD1A 0%, #9F391A 100%);*/
+        }
+        #menu-deroulant li:last-child{
+        background-color: #e7e7e7;
 
-}
-/* background des liens sous menus */
-#menu-deroulant li:first-child li{
-background:#e7e7e7;
-}
-#menu-deroulant li:nth-child(2) li{
-background:#e7e7e7;
-}
-#menu-deroulant li:nth-child(3) li{
-background:#e7e7e7;
-}
-#menu-deroulant li:last-child li{
-background:#e7e7e7;
-}
-/* background des liens menus et sous menus au survol */
+        }
+        /* background des liens sous menus */
+        #menu-deroulant li:first-child li{
+        background:#e7e7e7;
+        }
+        #menu-deroulant li:nth-child(2) li{
+        background:#e7e7e7;
+        }
+        #menu-deroulant li:nth-child(3) li{
+        background:#e7e7e7;
+        }
+        #menu-deroulant li:last-child li{
+        background:#e7e7e7;
+        }
+        /* background des liens menus et sous menus au survol */
 
-#menu-deroulant li:last-child:hover, #menu-deroulant li:last-child li:hover{
-background:#e0e0e0;
-}
-/* les a href */
-#menu-deroulant a{
-text-decoration:none;
-display:block;
-padding:8px 32px;
-color:#000;
-font-family:arial;
-}
-#menu-deroulant ul a{
-padding:8px 0;
-}
-#menu-deroulant li:hover li a{
-color:#000;
-text-transform:inherit;
-}
-#menu-deroulant li:hover a, #menu-deroulant li li:hover a{
-color:#fff;
-}
+        #menu-deroulant li:last-child:hover, #menu-deroulant li:last-child li:hover{
+        background:#e0e0e0;
+        }
+        /* les a href */
+        #menu-deroulant a{
+        text-decoration:none;
+        display:block;
+        padding:8px 32px;
+        color:#000;
+        font-family:arial;
+        }
+        #menu-deroulant ul a{
+        padding:8px 0;
+        }
+        #menu-deroulant li:hover li a{
+        color:#000;
+        text-transform:inherit;
+        }
+        #menu-deroulant li:hover a, #menu-deroulant li li:hover a{
+        color:#fff;
+        }
 
-/*----------------------------------------------------------------------*/
+        /*----------------------------------------------------------------------*/
 
 
     </style>
-
-    <title>VINY</title>
+    <title>Le Viny - Shop</title>
   </head>
   <body>
     <div class="top">
@@ -179,10 +178,12 @@ color:#fff;
 
 
         foreach($this->list as $id => $url) {
+          $pos = strpos($id, "non");
+          if ($pos === false) {
           echo "<a href=\"../Controler/thisvinyle.ctrl.php?id=", $id, "&firstId=", $this->firstId,"&genre=",$this->genre, "\">
                   <img src=\"http://$url\" alt=\"\" style = \"width:200px; height:200px\">
                 </a>";
-
+          }
         }
 
       echo "</div>";
@@ -191,7 +192,7 @@ color:#fff;
                   <a href=\"?firstId=",$this->prev,"&genre=",$this->genre,"\">
                     <img class=\"arrow\" src=\"../View/img/gauche.png\" alt=\"\">
                   </a>
-                  $this->currentPage
+                  $this->currentPage / $this->nbpagetotal
                   <a href=\"?firstId=",$this->next,"&genre=",$this->genre,"\">
                     <img class=\"arrow\" src=\"../View/img/droite.png\" alt=\"\">
                   </a>
